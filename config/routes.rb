22b64a8d49dccb59721cd082484693609e127e72
austinb847 <austinb847@icloud.com>
2020-06-01 11:39:48 -0700

    add bcrpyt gem, add new users model, add new controllers to handle user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,12 @@ Rails.application.routes.draw do
   resources :tags do
     resources :recipes
   end
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+
 end
